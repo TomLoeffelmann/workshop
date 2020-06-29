02cefd5c6755f339db5f2a170e66fce055735693
Tom Löffelmann <tom.loeffelmann@parshipgroup.com>
2020-06-29 17:57:16 +0200

    initial project setup

---
+++ workshop/workshop/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  workshop
+//
+//  Created by Tom Löffelmann on 29.06.20.
+//  Copyright © 2020 Tom Löffelmann. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ workshop/workshop/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  workshop
+//
+//  Created by Tom Löffelmann on 29.06.20.
+//  Copyright © 2020 Tom Löffelmann. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
